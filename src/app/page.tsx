10ebf7feee52bbeb18d586555637c7bfc3903fc1
princedev3 <marvinprince232@gmail.com>
2024-11-12 01:31:56 +0200

    hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Hero from "@/components/hero";
 import { Header } from "@/components/ui/header";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Hero />
     </div>
   );
 }
