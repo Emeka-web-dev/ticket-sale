3369251e51d536c79028497cfbfa0ead1b5197e2
Emeka-web-dev <chukwurex042@gmail.com>
2024-11-20 17:35:57 -0800

    feature: reset password form

---
+++ src/components/auth/new-password-form.tsx
@@ -0,0 +1,91 @@
+"use client";
+import { useSearchParams } from "next/navigation";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { CardWrapper } from "./card-wrapper";
+import { useState, useTransition } from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { NewPasswordSchema } from "../../../schemas";
+import { Input } from "../ui/input";
+import { FormError } from "../form-error";
+import { FormSuccess } from "../form-success";
+import { Button } from "../ui/button";
+import { Loader2 } from "lucide-react";
+import { newPassword } from "@/action/new-password-action";
+
+export const NewPasswordForm = () => {
+  const searchParams = useSearchParams();
+  const token = searchParams?.get("token");
+
+  const [error, setError] = useState<string | undefined>("");
+  const [success, setSuccess] = useState<string | undefined>("");
+  const [isPending, startTransition] = useTransition();
+  const form = useForm<z.infer<typeof NewPasswordSchema>>({
+    resolver: zodResolver(NewPasswordSchema),
+    defaultValues: {
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof NewPasswordSchema>) => {
+    setError("");
+    setSuccess("");
+
+    startTransition(() => {
+      newPassword(values, token).then((data) => {
+        form.reset();
+        setError(data?.error);
+        setSuccess(data?.success);
+      });
+    });
+  };
+  return (
+    <CardWrapper
+      headerLabel="Enter a new password"
+      backButtonLabel="Back to login"
+      backButtonHref="/auth/login"
+    >
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>New Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      disabled={isPending}
+                      placeholder="******"
+                      type="password"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormError message={error} />
+          <FormSuccess message={success} />
+          <Button type="submit" className="w-full" disabled={isPending}>
+            {isPending ? (
+              <Loader2 className="size-5 animate-spin" />
+            ) : (
+              "Reset password"
+            )}
+          </Button>
+        </form>
+      </Form>
+    </CardWrapper>
+  );
+};
--- src/middleware.ts
+++ src/middleware.ts
@@ -8,7 +8,8 @@ const { auth: middleware } = NextAuth(authConfig);
 
 export default middleware(async (req) => {
   const user = await currentUser();
-  const role = user?.role;
+  // const role = user?.role;
+  const isLoggedIn = req.auth;
 
   const { nextUrl } = req;
 
@@ -19,6 +20,36 @@ export default middleware(async (req) => {
   if (isApiRoute) return;
 
   if (isPublicRoute) return;
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      // If user is admin or moderator
+      // if (isAdminOrModerator) {
+      //   return Response.redirect(new URL("/admin/dashboard", nextUrl));
+      // }
+
+      return Response.redirect(new URL("/dashboard", nextUrl));
+    }
+    return;
+  }
+
+  // Check if session expires
+  if (
+    isLoggedIn &&
+    (user?.customExpiration as number) < Math.floor(Date.now() / 1000)
+  ) {
+    if (nextUrl.pathname !== "/logout") {
+      return Response.redirect(new URL("/logout", nextUrl));
+    }
+  }
+
+  // check if user is not logged in
+
+  if (!isLoggedIn) {
+    return Response.redirect(new URL("/auth/login", nextUrl));
+  }
+
+  return;
 });
 
 export const config = {
