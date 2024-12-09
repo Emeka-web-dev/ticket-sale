e546c83f5db8ca3adcbab262d07ac8b6a36de241
princedev3 <marvinprince232@gmail.com>
2024-12-09 03:19:50 +0200

    dashboard

--- src/app/(user)/dashboard/page.tsx
+++ src/app/(user)/dashboard/page.tsx
@@ -1,51 +1,8 @@
 "use client";
+import RealChartUI from "@/components/dashboard/real-chart";
 
 const DashboardPage = () => {
-  return (
-    <div className=" w-full min-h-[calc(100vh-100px)] p-4">
-      <h2>dashboard</h2>
-      {/* <DataTable columns={columns} data={dashboardPageData} /> */}
-    </div>
-  );
+  return <RealChartUI />;
 };
 
 export default DashboardPage;
-
-// const dashboardPageData: itemType = [
-//   {
-//     id: 1,
-//     title: "summary",
-//     Icon: Settings,
-//   },
-//   {
-//     id: 3,
-//     title: "sales",
-//     Icon: BadgeDollarSign,
-//   },
-//   {
-//     id: 3,
-//     title: "customers",
-//     Icon: User,
-//   },
-// ];
-
-// const listElements = [
-//   {
-//     id: 1,
-//     title: "sales",
-//     count: 120,
-//     Icon: BadgeDollarSign,
-//   },
-//   {
-//     id: 2,
-//     count: 140,
-//     title: "customers",
-//     Icon: User,
-//   },
-//   {
-//     id: 3,
-//     title: "Revenue",
-//     count: 12000,
-//     Icon: Settings,
-//   },
-// ];
