5682b4e76d7adfaabe490d9ddbcc401056fda8c8
Emeka-web-dev <chukwurex042@gmail.com>
2024-12-16 04:04:51 +0100

    webhook2

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -23,23 +23,6 @@ const AdminPage = () => {
     <div className="">
       <h1 className="text-3xl font-bold my-4 ">Manage your sales</h1>
       <div className="grid grid-cols-1 gap-4">
-        {/* <ResponsiveContainer className="" width="100%" minHeight={500}>
-            <BarChart width={150} height={40} data={data}>
-              <CartesianGrid stroke="#faf7f7" className="bg-gray-300" />
-              <XAxis dataKey="name" />
-              <YAxis stroke="#757171" />
-              <Tooltip
-                cursor={{ fill: "#d1d5db" }}
-                formatter={(value) => `$${value}k`}
-              />
-              <Bar
-                dataKey="uv"
-                fill="#9ca3af"
-                type="monotone"
-                stroke="#9ca3af"
-              />
-            </BarChart>
-          </ResponsiveContainer> */}
         <div className="">
           <AdminTableUI data={data} />
         </div>
